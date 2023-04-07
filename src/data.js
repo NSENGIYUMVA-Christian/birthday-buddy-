9f951dceba21a858803f61d189613723cde4886d
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-04-07 10:42:10 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "NSENGIYUMVA CHRISTIAN",
+    age: 29,
+    image:
+      "https://media.licdn.com/dms/image/D4D03AQFFov7JZu54Qg/profile-displayphoto-shrink_800_800/0/1667823935550?e=1686182400&v=beta&t=aewze2U4x5Pyb3TbvSgVIr2BT6YCoMdzEDH20zJwRy0",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
